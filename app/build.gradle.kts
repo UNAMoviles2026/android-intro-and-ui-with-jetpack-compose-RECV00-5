7172ad38de6f4bc0adb884a0ccc119f8ad55d4b7
RECV <134547520+RECV00@users.noreply.github.com>
2026-04-24 21:03:42 -0600

    Laboratorio 4 completado, arreglos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.compose.foundation)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
